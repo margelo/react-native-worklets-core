ee4650848b9c86404eef6d9a4cb02841f6330945
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-04-03 15:39:45 +0200

    reproduction example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,84 +1,22 @@
-import React from "react";
-import {
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
-
-import { useTestRunner } from "../Tests";
-import { TestWrapper } from "../Tests/TestWrapper";
+import { useEffect } from "react";
+import { Worklets } from "react-native-worklets-core";
 
 const App = () => {
-  const { tests, categories, output, runTests, runSingleTest } =
-    useTestRunner();
-  return (
-    <View style={styles.container}>
-      <View style={styles.tests}>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          contentContainerStyle={styles.testsScrollView}
-        >
-          {categories.map((category) => (
-            <React.Fragment key={category}>
-              <TouchableOpacity
-                onPress={() =>
-                  runTests(tests.filter((t) => t.category === category))
-                }
-              >
-                <Text style={styles.category}>{category}</Text>
-              </TouchableOpacity>
-              {tests
-                .filter((t) => t.category === category)
-                .map((t) => (
-                  <TestWrapper
-                    key={t.name}
-                    name={t.name}
-                    state={t.state}
-                    onRun={() => runSingleTest(t)}
-                  />
-                ))}
-            </React.Fragment>
-          ))}
-        </ScrollView>
-      </View>
-      <View style={styles.output}>
-        <ScrollView>
-          <Text style={styles.outputText}>{output.join("\n")}</Text>
-        </ScrollView>
-      </View>
-    </View>
-  );
-};
+  useEffect(() => {
+    const workeltFunctionA = () => {
+      "worklet";
+    };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  tests: {
-    paddingTop: 34,
-    flex: 1,
-  },
-  testsScrollView: {
-    paddingTop: 24,
-    paddingBottom: 40,
-  },
-  output: {
-    borderTopColor: "#CCC",
-    borderTopWidth: StyleSheet.hairlineWidth,
-    flex: 0.2,
-    backgroundColor: "#EFEFEF",
-  },
-  outputText: {
-    padding: 8,
-  },
-  category: {
-    paddingTop: 8,
-    paddingBottom: 4,
-    paddingHorizontal: 8,
-    textDecorationLine: "underline",
-  },
-});
+    Worklets.createRunInContextFn(() => {
+      "worklet";
+      function innerFct() {
+        workeltFunctionA();
+      }
+      innerFct();
+    });
+  }, []);
+
+  return null;
+};
 
 export default App;
