a37b3af0d2be83ec37dec833efb5541a01bbbd89
Marc Rousavy <me@mrousavy.com>
2024-05-15 14:14:53 +0200

    Update useRunOnJS.ts

--- package/src/hooks/useRunOnJS.ts
+++ package/src/hooks/useRunOnJS.ts
@@ -1,4 +1,5 @@
-import { DependencyList, useMemo } from "react";
+import type { DependencyList } from "react";
+import { useMemo } from "react";
 import { Worklets } from "../NativeWorklets";
 
 /**
