eed7c0f5cfb3afeed04abffe0fecca69c5a92920
Marc Rousavy <me@mrousavy.com>
2024-05-15 14:10:48 +0200

    fix: Fix configs

--- package/src/ModuleNotFoundError.ts
+++ package/src/ModuleNotFoundError.ts
@@ -1,11 +1,11 @@
-import { NativeModules, Platform } from 'react-native';
+import { NativeModules, Platform } from "react-native";
 
 declare global {
   // A react-native internal from TurboModuleRegistry.js
   var __turboModuleProxy: unknown | undefined;
 }
 
-const BULLET_POINT = '\n* ';
+const BULLET_POINT = "\n* ";
 
 function messageWithSuggestions(
   message: string,
@@ -14,20 +14,20 @@ function messageWithSuggestions(
   return message + BULLET_POINT + suggestions.join(BULLET_POINT);
 }
 
-function getFrameworkType(): 'react-native' | 'expo' | 'expo-go' {
+function getFrameworkType(): "react-native" | "expo" | "expo-go" {
   // check if Expo
   const ExpoConstants =
     NativeModules.NativeUnimoduleProxy?.modulesConstants?.ExponentConstants;
   if (ExpoConstants != null) {
-    if (ExpoConstants.appOwnership === 'expo') {
+    if (ExpoConstants.appOwnership === "expo") {
       // We're running Expo Go
-      return 'expo-go';
+      return "expo-go";
     } else {
       // We're running Expo bare / standalone
-      return 'expo';
+      return "expo";
     }
   }
-  return 'react-native';
+  return "react-native";
 }
 
 export class ModuleNotFoundError extends Error {
@@ -38,13 +38,13 @@ export class ModuleNotFoundError extends Error {
       // react-native-mmkv 3.x.x requires new arch (react-native >0.74)
       // react-native-mmkv 2.x.x works on old arch (react-native <0.74)
       const message =
-        'Failed to initialize react-native-worklets-core: react-native-worklets-core 2.x.x requires TurboModules, but the new architecture is not enabled!';
+        "Failed to initialize react-native-worklets-core: react-native-worklets-core 2.x.x requires TurboModules, but the new architecture is not enabled!";
       const suggestions: string[] = [];
       suggestions.push(
-        'Downgrade to react-native-worklets-core 1.x.x if you want to stay on the old architecture.'
+        "Downgrade to react-native-worklets-core 1.x.x if you want to stay on the old architecture."
       );
       suggestions.push(
-        'Enable the new architecture in your app to use react-native-worklets-core 2.x.x. (See https://github.com/reactwg/react-native-new-architecture/blob/main/docs/enable-apps.md)'
+        "Enable the new architecture in your app to use react-native-worklets-core 2.x.x. (See https://github.com/reactwg/react-native-new-architecture/blob/main/docs/enable-apps.md)"
       );
       const error = messageWithSuggestions(message, suggestions);
       super(error, { cause: cause });
@@ -52,41 +52,43 @@ export class ModuleNotFoundError extends Error {
     }
 
     const framework = getFrameworkType();
-    if (framework === 'expo-go') {
+    if (framework === "expo-go") {
       super(
-        'react-native-worklets-core is not supported in Expo Go! Use EAS (`expo prebuild`) or eject to a bare workflow instead.'
+        "react-native-worklets-core is not supported in Expo Go! Use EAS (`expo prebuild`) or eject to a bare workflow instead."
       );
       return;
     }
 
     const message =
-      'Failed to initialize react-native-worklets-core: The native RNWorklets CxxTurboModule cannot be found.';
+      "Failed to initialize react-native-worklets-core: The native RNWorklets CxxTurboModule cannot be found.";
     const suggestions: string[] = [];
     suggestions.push(
-      'Make sure react-native-worklets-core is correctly autolinked (run `npx react-native config` to verify)'
+      "Make sure react-native-worklets-core is correctly autolinked (run `npx react-native config` to verify)"
     );
     suggestions.push(
-      'Make sure you enabled the new architecture (TurboModules) and CodeGen properly generated the react-native-worklets-core specs. See https://github.com/reactwg/react-native-new-architecture/blob/main/docs/enable-apps.md'
+      "Make sure you enabled the new architecture (TurboModules) and CodeGen properly generated the react-native-worklets-core specs. See https://github.com/reactwg/react-native-new-architecture/blob/main/docs/enable-apps.md"
     );
     suggestions.push(
-      'Make sure you are using react-native 0.74.0 or higher, because react-native-worklets-core is a C++ TurboModule.'
+      "Make sure you are using react-native 0.74.0 or higher, because react-native-worklets-core is a C++ TurboModule."
     );
-    suggestions.push('Make sure you rebuilt the app.');
-    if (framework === 'expo') {
-      suggestions.push('Make sure you ran `expo prebuild`.');
+    suggestions.push("Make sure you rebuilt the app.");
+    if (framework === "expo") {
+      suggestions.push("Make sure you ran `expo prebuild`.");
     }
     switch (Platform.OS) {
-      case 'ios':
-      case 'macos':
+      case "ios":
+      case "macos":
         suggestions.push(
-          'Make sure you ran `pod install` in the ios/ directory.'
+          "Make sure you ran `pod install` in the ios/ directory."
         );
         break;
-      case 'android':
-        suggestions.push('Make sure gradle is synced.');
+      case "android":
+        suggestions.push("Make sure gradle is synced.");
         break;
       default:
-        suggestions.push(`Make sure react-native-worklets-core is supported on ${Platform.OS}.`)
+        suggestions.push(
+          `Make sure react-native-worklets-core is supported on ${Platform.OS}.`
+        );
         break;
     }
 
--- package/src/NativeWorklets.ts
+++ package/src/NativeWorklets.ts
@@ -16,7 +16,7 @@ export interface Spec extends TurboModule {
   createWorkletsApi(): UnsafeObject;
 }
 
-let module: Spec
+let module: Spec;
 try {
   // Try to find the CxxTurboModule.
   // CxxTurboModules can be autolinked on Android starting from react-native 0.74,
@@ -24,7 +24,7 @@ try {
   module = TurboModuleRegistry.getEnforcing<Spec>("Worklets");
 } catch (e) {
   // User didn't enable new arch, or the module does not exist.
-  throw new ModuleNotFoundError(e)
+  throw new ModuleNotFoundError(e);
 }
 
 /**
@@ -32,8 +32,7 @@ try {
  * This object can be shared and accessed from multiple contexts,
  * however it is advised to not hold unnecessary references to it.
  */
-export const Worklets = module.createWorkletsApi() as IWorkletNativeApi
-
+export const Worklets = module.createWorkletsApi() as IWorkletNativeApi;
 
 if (__DEV__) {
   console.log("react-native-worklets-core loaded successfully!");
