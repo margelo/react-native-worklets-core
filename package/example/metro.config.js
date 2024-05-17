cf641561c635f179b75cf880f78240cdb6e281b7
Marc Rousavy <me@mrousavy.com>
2024-05-17 10:52:47 +0200

    Lint

--- package/example/metro.config.js
+++ package/example/metro.config.js
@@ -1,10 +1,10 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
-const path = require('path');
-const escape = require('escape-string-regexp');
-const exclusionList = require('metro-config/src/defaults/exclusionList');
-const pak = require('../package.json');
+const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
+const path = require("path");
+const escape = require("escape-string-regexp");
+const exclusionList = require("metro-config/src/defaults/exclusionList");
+const pak = require("../package.json");
 
-const root = path.resolve(__dirname, '..');
+const root = path.resolve(__dirname, "..");
 const modules = Object.keys({ ...pak.peerDependencies });
 
 /**
@@ -22,12 +22,12 @@ const config = {
     blacklistRE: exclusionList(
       modules.map(
         (m) =>
-          new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
+          new RegExp(`^${escape(path.join(root, "node_modules", m))}\\/.*$`)
       )
     ),
 
     extraNodeModules: modules.reduce((acc, name) => {
-      acc[name] = path.join(__dirname, 'node_modules', name);
+      acc[name] = path.join(__dirname, "node_modules", name);
       return acc;
     }, {}),
   },
