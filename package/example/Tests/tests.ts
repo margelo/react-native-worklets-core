552186a62b688c8e125e01fac51850a28086106a
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-06-05 16:42:17 +0200

    add test for native context creation

--- package/example/Tests/tests.ts
+++ package/example/Tests/tests.ts
@@ -2,9 +2,11 @@ import { worklet_tests } from "./worklet-tests";
 import { sharedvalue_tests } from "./sharedvalue-tests";
 import { wrapper_tests } from "./wrapper-tests";
 import { worklet_context_tests } from "./worklet-context-tests";
+import { native_contexts_tests } from "./native-contexts-tests";
 
 export const Tests: { [key: string]: { [key: string]: () => Promise<void> } } =
   {
+    NativeContexts: { ...native_contexts_tests },
     Worklets: { ...worklet_tests },
     Contexts: { ...worklet_context_tests },
     SharedValues: { ...sharedvalue_tests },
