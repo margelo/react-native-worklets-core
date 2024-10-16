00dd8b94e22acf16b73d561c2637f6dfa0d1d577
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-10-16 14:23:51 +0200

    wip android

---
+++ package/android/src/main/java/com/margelo/worklets/JNIOnLoad.java
@@ -0,0 +1,27 @@
+package com.margelo.worklets;
+
+import android.util.Log;
+
+public class JNIOnLoad {
+    private static final String TAG = "Worklets";
+    private static boolean isInitialized = false;
+
+    /**
+     * Initializes the C++ (JNI) part of react-native-worklets-core.
+     * This function should be called in a `static` block everytime the Java part
+     * interacts with the native C++ (JNI) part of react-native-worklets-core.
+     * If react-native-worklets-core is already initialized, this function will do nothing.
+     */
+    public synchronized static void initializeNativeWorklets() {
+        if (isInitialized) return;
+        try {
+            Log.i(TAG, "Loading react-native-worklets-core C++ library...");
+            System.loadLibrary("Worklets");
+            Log.i(TAG, "Successfully loaded react-native-worklets-core C++ library!");
+            isInitialized = true;
+        } catch (Throwable e) {
+            Log.e(TAG, "Failed to load react-native-worklets-core C++ library! Is it properly installed and linked?", e);
+            throw e;
+        }
+    }
+}
--- package/src/NativeWorklets.ts
+++ package/src/NativeWorklets.ts
@@ -1,7 +1,6 @@
 import type { TurboModule } from "react-native";
 import { TurboModuleRegistry } from "react-native";
 import type { IWorkletNativeApi } from "./types";
-import type { UnsafeObject } from "react-native/Libraries/Types/CodegenTypes";
 import { ModuleNotFoundError } from "./ModuleNotFoundError";
 
 if (__DEV__) {
@@ -10,21 +9,23 @@ if (__DEV__) {
 
 export interface Spec extends TurboModule {
   /**
-   * Create a new instance of the Worklets API.
-   * The returned {@linkcode UnsafeObject} is a `jsi::HostObject`.
+   * Installs `WorkletsProxy` into this JS Runtime's `global`.
+   * @returns A `string` error message if this function failed to run, or `undefined` if everything went successful.
    */
-  createWorkletsApi(): UnsafeObject;
+  install(): string | undefined;
 }
 
 let module: Spec;
 try {
-  // Try to find the CxxTurboModule.
-  // CxxTurboModules can be autolinked on Android starting from react-native 0.74,
-  // and are manually linked in WorkletsOnLoad.mm on iOS.
-  module = TurboModuleRegistry.getEnforcing<Spec>("WorkletsCxx");
+  module = TurboModuleRegistry.getEnforcing<Spec>("Worklets");
 } catch (e) {
   // User didn't enable new arch, or the module does not exist.
   throw new ModuleNotFoundError(e);
+}
+
+const installResult = module.install();
+if (typeof installResult === "string") {
+  throw new Error(installResult);
 }
 
 /**
@@ -32,7 +33,7 @@ try {
  * This object can be shared and accessed from multiple contexts,
  * however it is advised to not hold unnecessary references to it.
  */
-export const Worklets = module.createWorkletsApi() as IWorkletNativeApi;
+export const Worklets = global.WorkletsProxy as IWorkletNativeApi; // TODO: actually inject that into global
 
 if (__DEV__) {
   console.log("react-native-worklets-core loaded successfully!");
