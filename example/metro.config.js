155eec482fead82328857715f60d8b97281106a7
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-11-06 14:35:43 +0100

    fix eslint setup

--- example/metro.config.js
+++ example/metro.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const escape = require("escape-string-regexp");
 const exclusionList = require("metro-config/src/defaults/exclusionList");
 const pak = require("../package.json");
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
 
 const root = path.resolve(__dirname, "..");
 
